fa2c127de89926482ebaea24442430d6032d633c
apandey9524 <apandey@tavisca.com>
2018-06-01 07:03:11 +0530

    fixed jenkins install

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -21,7 +21,7 @@ sudo apt-get install dotnet-sdk-2.1.200 -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 
 #disable security for jenkins and restart
 service jenkins stop
