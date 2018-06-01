d235f4022aab89d7909164a4f10b549169bcec21
apandey9524 <apandey@tavisca.com>
2018-06-01 14:31:50 +0530

    added jenkins firewall exception

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -22,16 +22,19 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
-
+sudo ufw allow 8080
 #disable security for jenkins and restart
-service jenkins stop
-sudo sed -i "s/<useSecurity>true/<useSecurity>false/g" /var/lib/jenkins/config.xml
-sudo sed -i "s/<authorizationStrategy/<\!--authorizationStrategy/g" /var/lib/jenkins/config.xml
-sudo sed -i "s/securityRealm>/securityRealm-->/g" /var/lib/jenkins/config.xml
-service jenkins start
+sudo service jenkins stop
+# sudo sed -i "s/<useSecurity>true/<useSecurity>false/g" /var/lib/jenkins/config.xml
+# sudo sed -i "s/<authorizationStrategy/<\!--authorizationStrategy/g" /var/lib/jenkins/config.xml
+# sudo sed -i "s/securityRealm>/securityRealm-->/g" /var/lib/jenkins/config.xml
+git clone https://github.com/apandey9524/jenkins-config.git
+cp -f jenkins-config/config.xml /var/lib/jenkins/config.xml
+sudo service jenkins start
 wget -q --auth-no-challenge --user USERNAME --password PASSWORD --output-document crumb.txt 'http://localhost:8080//crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'
 crumbid=$(<crumb.txt)
-curl -X POST -H "Jenkins-Crumb:$crumbid" -d "" http://localhost:8080/setupWizard/completeInstall
+
+sudo curl -X POST -H "$crumbid" -d "" http://localhost:8080/setupWizard/completeInstall
 
 #create list of plugins to be installed and install plugins
 pluginlist = "$1"
