a0da6b27fcc186c84d9b4f82c0097aa2a680273e
apandey9524 <apandey@tavisca.com>
2018-06-01 17:28:01 +0530

    dfgadfg

--- createJobConfig.sh
+++ createJobConfig.sh
@@ -6,8 +6,8 @@ TARGETFILEPATH="$3"
 MAILINGLIST="$4"
 git clone https://github.com/apandey9524/jenkin-job-templates.git
 cd jenkin-job-templates
-git checkout -b dotnetcore-job-template
-cat template-config.xml>>job-config.xml
+git checkout dotnetcore-job-template
+cp -f template-config.xml job-config.xml
 sudo sed -i "s/\-\-GITREPO\-\-//g${GITREPO}" job-config.xml
 sudo sed -i "s/\-\-GITBRANCHNAME\-\-//g${GITBRANCHNAME}" job-config.xml
 sudo sed -i "s/\-\-TARGETFILEPATH\-\-//g${TARGETFILEPATH}" job-config.xml
