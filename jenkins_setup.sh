001878ae64ff8dd9b8e0f112ed8b8f4d30e00a17
apandey9524 <apandey@tavisca.com>
2018-06-04 15:03:54 +0530

    skjdfsdf

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # #install oracle java 8 and set it default java tool
-# sudo add-apt-repository ppa:webupd8team/java -y
-# sudo apt-get update
-# echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-# sudo apt-get install oracle-java8-installer -y
-# sudo apt-get install oracle-java8-set-default
+ sudo add-apt-repository ppa:webupd8team/java -y
+ sudo apt-get update
+ echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+ sudo apt-get install oracle-java8-installer -y
+ sudo apt-get install oracle-java8-set-default
 
 # #install git
 sudo apt install git
